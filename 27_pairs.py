3cbcaaec89e17300a2be2dad3e2931c780ae8cce
Admin <mavlav2005@gmail.com>
2022-04-25 17:26:19 +0500

    Add file

--- 27_pairs.py
+++ 27_pairs.py
@@ -1,25 +1,29 @@
 #Набор данных состоит из пар натуральных чисел. Необходимо выбрать из набора некоторые пары так, чтобы второе число в каждой выбранной паре было нечётным, сумма бо́льших чисел во всех выбранных парах была чётной, а сумма меньших – нечётной. Какую наибольшую сумму чисел во всех выбранных парах можно при этом получить?
-a = open('numbers').read().splitlines()
+f = open('numbers')
+a = f.read().splitlines()[1:]
 mx = 0
 mn = 0
-x0 = []
-x1 = []
+min2 = []#макс четное мин нечетное
+min1 = []#оба нечетные
+min3 = []# макс нечетное мин четное
 for i in a:
     x, y = map(int,i.split())
-    if y % 2 != 0:
+    if x % 2 == 1:
         mx += max(x,y)
         mn += min(x,y)
         if max(x,y) % 2 == 0:
-            x0.append(x + y)
+            min2.append(x + y)
         else:
-            x1.append(x + y)
-x0.sort()
-x1.sort()
-if mx % 2 == 0 and mn % 2 == 0:
-    print(mx + mn - min(x0))
-if mx % 2 == 1 and mn % 2 == 1:
-    print(mx + mn - min(x0) - min(x1))
+            if min(x,y) % 2 == 0:
+                min3.append(x + y)
+            else:
+                min1.append(x + y)
+summa = mx + mn
 if mx % 2 == 1 and mn % 2 == 0:
-    print(mx + mn - min(x1))
+    print(summa)
 if mx % 2 == 0 and mn % 2 == 1:
-    print(max(mx + mn - x1[0] - x1[1]),(mx + mn - x0[0] - x0[1]))
+    print(max(summa - min(min1),summa - min(min2)- min(min3)))
+if mx % 2 == 1 and mn % 2 == 1:
+    print(max(summa - min(min2), summa - min(min1) - min(min3)))
+if mx % 2 == 0 and mn % 2 == 0:
+    print(max(summa - min(min3), summa - min(min1) - min(min2)))
